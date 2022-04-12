b5f0b4356c1929a3d70e070f7517f43b13d2fb47
smduarte <smd@fct.unl.pt>
2022-04-12 23:27:39 +0100

    Update publisher.py

--- colab/socket-tp1-logsender/publisher.py
+++ colab/socket-tp1-logsender/publisher.py
@@ -75,7 +75,7 @@ def handleClient(conn, lines, speedup) :
                         
                 dt = parse( line )
                 
-                data = '{} {} {} {} {} {} {}\n'.format(dt.sensor_id, dt.sensor_type, dt.location, dt.latitude, dt.longitude, dt.p1, dt.p2)
+                data = '{} {} {} {} {} {} {} {}\n'.format(dt.sensor_id, dt.sensor_type, dt.location, dt.latitude, dt.longitude, dt.p1, dt.p2, dt.timestamp)
                 
                 conn.sendall( data.encode() )
                 
